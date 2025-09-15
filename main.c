81cbf895b98d9ac1c9aa4d31f35e5f8cd9690a02
jason dawn <hdviews.01@gmail.com>
2025-09-15 16:50:44 -0400

    added the timer for the balls to spawn the balls

--- main.c
+++ main.c
@@ -14,40 +14,62 @@ int main(void) {
         int screenWidth = 480;
         int score = 0;
         int specialMove = 2;
-        struct entity Player = { 0, 0, 25, RED, true };
-        struct entity Enemy = { 0, 0, 40, GREEN, true};
+        int enemyCounter = 0;
+        unsigned int framesCounter = 0;
+        struct entity Player = { 0, 0, 25, RED, true};
+        struct entity Enemy[10] = { 0, 0, 40, GREEN, false};
+        
+        //spreading the enemies against the top of the screen
+        for (int i = 0; i < 10; i++) {
+                Enemy[i].x = GetRandomValue(0, screenHeight);
+                printf("%d", Enemy[i].x);
+        }
 
         InitWindow(screenHeight, screenWidth, "falling balls");
+        SetTargetFPS(60);
 
         while(!WindowShouldClose()) {
+
+                //player position is the mouse position
                 Player.x = GetMouseX();
                 Player.y = GetMouseY();
-                Enemy.x = 200;
-                Enemy.y = 200;
+                framesCounter++;
 
-                if (CheckCollisionCircles((Vector2){Player.x, Player.y}, Player.radius, (Vector2){Enemy.x, Enemy.y}, Enemy.radius)) {
-                        Enemy.color = DARKPURPLE;
-                        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                                Enemy.active = false;
-                                score += 1;
+                //every 5 seconds summon an enemy
+                if (((framesCounter/300)%5) == 1) {
+                        Enemy[enemyCounter].active = true;
+                        enemyCounter += 1;
+                        framesCounter = 0;
+                }
+                        
+
+                for(int i = 0; i < 10; i++) {
+                        if (Enemy[i].active == true) {
+                                Enemy[i].y += 1;
+                        //if the player and the enemy collide
+                        if (CheckCollisionCircles((Vector2){Player.x, Player.y}, Player.radius, (Vector2){Enemy[i].x, Enemy[i].y}, Enemy[i].radius)) {
+                                Enemy[i].color = DARKPURPLE;
+                                if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                                        Enemy[i].active = false;
+                                        score += 1;
+                                }
+                        }
                         }
                 }
 
 
-
                 BeginDrawing();
                         ClearBackground(RAYWHITE);
+                        //text drawings
                         DrawFPS(10, 25);
                         DrawText(TextFormat("Score: %i", score), 10, 50, 15, BLACK); 
-                        DrawText("falling balls",
-                                        190,
-                                        200,
-                                        20,
-                                        BLACK);
+                        //entity drawings
                         DrawCircle(Player.x, Player.y, Player.radius, Player.color);
-                        if (Enemy.active == true) {
-                                DrawCircle(Enemy.x, Enemy.y, Enemy.radius, Enemy.color);
-                }
+                        for(int i = 0; i < 10; i++) {
+                                        if (Enemy[i].active == true) {
+                                                DrawCircle(Enemy[i].x, Enemy[i].y, Enemy[i].radius, Enemy[i].color);
+                                }
+                        }
                 EndDrawing();
         }
 
